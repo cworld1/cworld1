0dbbcd76e684d4893fe49c02930d2f557860b5d3
cworld1 <cworld0@qq.com>
2022-02-15 17:40:58 +0800

    Update some stsaic source

--- source/static/js/main.js
+++
@@ -1,6 +0,0 @@
-var elements = document.querySelectorAll("is-i.φce, header.φcv, div.φdl");
-console.log('@', elements[0]);
-for (var i = 0; i < elements.length; i++) {
-    element = elements[i];
-    element.classList.add("ripple");
-};
