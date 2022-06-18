c7225689d52e9ab6a3c8db0c609092a2e89ba1d3
cworld1 <cworld0@qq.com>
2022-06-18 16:37:07 +0800

    fix(js): fix bugs that cause some error

--- source/static/js/about.js
+++ source/static/js/about.js
@@ -797,7 +797,7 @@ var _typeof2 =
               ]),
               u);
           function u(e) {
-            !(function (e, t) {
+            (function (e, t) {
               if (!(e instanceof t))
                 throw new TypeError("Cannot call a class as a function");
             })(this, u),
