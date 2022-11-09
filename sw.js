281906172dfa1968e0cd0f68e1946e3b9168b4b3
cworld1 <cworld0@qq.com>
2022-11-09 02:54:07 +0000

    Merge pull request #21 from cworld1/dependabot/npm_and_yarn/hexo-generator-archive-2.0.0
    build(deps): bump hexo-generator-archive 从 1.0.0 到 2.0.0。

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ workbox.setConfig({ modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.6.3/' }
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.keys().then(function (names) {
-      var validSets = ["is-sw-905032","is-html-905032","is-jsdelivr-905032","is-theme-905032","is-json-905032"];
+      var validSets = ["is-sw-adbd29","is-html-adbd29","is-jsdelivr-adbd29","is-theme-adbd29","is-json-adbd29"];
       return Promise.all(
         names
           .filter(function (name) { return !~validSets.indexOf(name); })
@@ -18,16 +18,16 @@ self.addEventListener('install', function (event) {
 });
 
 workbox.routing.registerRoute(new RegExp('sw\\.js'), workbox.strategies.networkOnly({
-  cacheName: 'is-sw-905032',
+  cacheName: 'is-sw-adbd29',
 }));
 workbox.routing.registerRoute(new RegExp('https://cdn\\.jsdelivr\\.net'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-jsdelivr-905032',
+  cacheName: 'is-jsdelivr-adbd29',
 }));
 workbox.routing.registerRoute(new RegExp('/.*\\.(?:js|css|woff2|png|jpg|gif)l;/span>'), workbox.strategies.cacheFirst({
-  cacheName: 'is-theme-905032',
+  cacheName: 'is-theme-adbd29',
 }));
 workbox.routing.registerRoute(new RegExp('your_data_prefix/.*\\.json'), workbox.strategies.cacheFirst({
-  cacheName: 'is-json-905032',
+  cacheName: 'is-json-adbd29',
 }));
 
 workbox.routing.registerRoute(new RegExp('/.*(:?/[^\\.]*/?)$'), function(context) {
