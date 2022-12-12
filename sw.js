a5d68a38ae1e521000e8d23afbc611813afe1676
cworld1 <cworld0@qq.com>
2022-12-12 18:02:02 +0000

    fix(static): :recycle: reback bilibili emojis

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ workbox.setConfig({ modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.6.3/' }
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.keys().then(function (names) {
-      var validSets = ["is-sw-40f75d","is-html-40f75d","is-jsdelivr-40f75d","is-theme-40f75d","is-json-40f75d"];
+      var validSets = ["is-sw-24fccb","is-html-24fccb","is-jsdelivr-24fccb","is-theme-24fccb","is-json-24fccb"];
       return Promise.all(
         names
           .filter(function (name) { return !~validSets.indexOf(name); })
@@ -18,16 +18,16 @@ self.addEventListener('install', function (event) {
 });
 
 workbox.routing.registerRoute(new RegExp('sw\\.js'), workbox.strategies.networkOnly({
-  cacheName: 'is-sw-40f75d',
+  cacheName: 'is-sw-24fccb',
 }));
 workbox.routing.registerRoute(new RegExp('https://cdn\\.jsdelivr\\.net'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-jsdelivr-40f75d',
+  cacheName: 'is-jsdelivr-24fccb',
 }));
 workbox.routing.registerRoute(new RegExp('/.*\\.(?:js|css|woff2|png|jpg|gif)l;/span>'), workbox.strategies.cacheFirst({
-  cacheName: 'is-theme-40f75d',
+  cacheName: 'is-theme-24fccb',
 }));
 workbox.routing.registerRoute(new RegExp('your_data_prefix/.*\\.json'), workbox.strategies.cacheFirst({
-  cacheName: 'is-json-40f75d',
+  cacheName: 'is-json-24fccb',
 }));
 
 workbox.routing.registerRoute(new RegExp('/.*(:?/[^\\.]*/?)$'), function(context) {
