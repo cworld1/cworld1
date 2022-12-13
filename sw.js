bce5ae7d46959047069ffbb39c6d6d053580f45c
cworld1 <cworld0@qq.com>
2022-12-13 17:10:26 +0000

    feat(config.inside): :rocket: fix cdn problem; update pwa support; resize waline.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ workbox.setConfig({ modulePathPrefix: 'https://cdn.cbd.int/workbox-sw@6.5.4/buil
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.keys().then(function (names) {
-      var validSets = ["is-sw-0839ec","is-html-0839ec","is-jsdelivr-0839ec","is-theme-0839ec","is-json-0839ec"];
+      var validSets = ["is-sw-f64ca9","is-html-f64ca9","is-jsdelivr-f64ca9","is-theme-f64ca9","is-json-f64ca9"];
       return Promise.all(
         names
           .filter(function (name) { return !~validSets.indexOf(name); })
@@ -18,18 +18,18 @@ self.addEventListener('install', function (event) {
 });
 
 workbox.routing.registerRoute(new RegExp('sw\\.js'), workbox.strategies.networkOnly({
-  cacheName: 'is-sw-0839ec',
+  cacheName: 'is-sw-f64ca9',
 }));
 workbox.routing.registerRoute(new RegExp('https://gcore\\.jsdelivr\\.net'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-jsdelivr-0839ec',
+  cacheName: 'is-jsdelivr-f64ca9',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('/.*\\.(?:js|css|woff2|png|jpg|gif)l;/span>'), workbox.strategies.cacheFirst({
-  cacheName: 'is-theme-0839ec',
+  cacheName: 'is-theme-f64ca9',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('your_data_prefix/.*\\.json'), workbox.strategies.cacheFirst({
-  cacheName: 'is-json-0839ec',
+  cacheName: 'is-json-f64ca9',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 
