89e2756be11ef83404fd4da632a4f131a9ac2e53
cworld1 <cworld0@qq.com>
2023-01-28 16:54:21 +0000

    docs(link): :sparkles: add link 大蛋糕的烘焙坊

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ workbox.setConfig({ modulePathPrefix: 'https://storage.googleapis.com/workbox-cd
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.keys().then(function (names) {
-      var validSets = ["is-sw-5a9e71","is-html-5a9e71","is-cbd_cdn-5a9e71","is-theme-5a9e71","is-json-5a9e71"];
+      var validSets = ["is-sw-feb8df","is-html-feb8df","is-cbd_cdn-feb8df","is-theme-feb8df","is-json-feb8df"];
       return Promise.all(
         names
           .filter(function (name) { return !~validSets.indexOf(name); })
@@ -18,18 +18,18 @@ self.addEventListener('install', function (event) {
 });
 
 workbox.routing.registerRoute(new RegExp('sw\\.js'), workbox.strategies.networkOnly({
-  cacheName: 'is-sw-5a9e71',
+  cacheName: 'is-sw-feb8df',
 }));
 workbox.routing.registerRoute(new RegExp('https://cdn\\.cbd\\.int'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-cbd_cdn-5a9e71',
+  cacheName: 'is-cbd_cdn-feb8df',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('/.*\\.(?:js|css|woff2|png|jpg|gif)l;/span>'), workbox.strategies.cacheFirst({
-  cacheName: 'is-theme-5a9e71',
+  cacheName: 'is-theme-feb8df',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('your_data_prefix/.*\\.json'), workbox.strategies.cacheFirst({
-  cacheName: 'is-json-5a9e71',
+  cacheName: 'is-json-feb8df',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 
