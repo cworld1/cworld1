fa3924ba42613b1eb2a63017163a9037011ec303
cworld1 <cworld0@qq.com>
2023-03-01 04:41:49 +0000

    Merge branch 'main' of https://github.com/cworld1/cworld1

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ workbox.setConfig({ modulePathPrefix: 'https://storage.googleapis.com/workbox-cd
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.keys().then(function (names) {
-      var validSets = ["is-sw-8d71d8","is-html-8d71d8","is-cbd_cdn-8d71d8","is-theme-8d71d8","is-json-8d71d8"];
+      var validSets = ["is-sw-1396d0","is-html-1396d0","is-cbd_cdn-1396d0","is-theme-1396d0","is-json-1396d0"];
       return Promise.all(
         names
           .filter(function (name) { return !~validSets.indexOf(name); })
@@ -18,18 +18,18 @@ self.addEventListener('install', function (event) {
 });
 
 workbox.routing.registerRoute(new RegExp('sw\\.js'), workbox.strategies.networkOnly({
-  cacheName: 'is-sw-8d71d8',
+  cacheName: 'is-sw-1396d0',
 }));
 workbox.routing.registerRoute(new RegExp('https://cdn\\.cbd\\.int'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-cbd_cdn-8d71d8',
+  cacheName: 'is-cbd_cdn-1396d0',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('/.*\\.(?:js|css|woff2|png|jpg|gif)l;/span>'), workbox.strategies.cacheFirst({
-  cacheName: 'is-theme-8d71d8',
+  cacheName: 'is-theme-1396d0',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('your_data_prefix/.*\\.json'), workbox.strategies.cacheFirst({
-  cacheName: 'is-json-8d71d8',
+  cacheName: 'is-json-1396d0',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 
