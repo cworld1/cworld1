ad60234e607ae0c40863d4b859f3213482d16534
CWorld <cworld0@qq.com>
2024-05-08 21:35:22 +0800

    feat: :sparkles: better support of rss; add detailed content function

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,19 @@
 import rss from '@astrojs/rss'
 import { siteConfig } from '@/site-config'
 import { getAllPosts } from '@/utils'
+import sanitizeHtml from 'sanitize-html'
+import MarkdownIt from 'markdown-it'
+const parser = new MarkdownIt()
 
 export const GET = async () => {
   const posts = await getAllPosts()
 
   return rss({
+    // configs
+    trailingSlash: false,
+    stylesheet: '/pretty-feed-v3.xsl',
+
+    // contents
     title: siteConfig.title,
     description: siteConfig.description,
     site: import.meta.env.SITE,
@@ -13,7 +21,10 @@ export const GET = async () => {
       title: post.data.title,
       description: post.data.description,
       pubDate: post.data.publishDate,
-      link: `/blog/${post.slug}`
+      link: `/blog/${post.slug}`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      })
     }))
   })
 }
