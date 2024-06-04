68e0ecbc92c5245d8971cc9a4117e71b23512afc
CWorld <cworld0@qq.com>
2024-06-04 20:18:43 +0800

    feat(package): :sparkles: add playformcompress to compress web files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
+// Adapter
+import vercel from '@astrojs/vercel/serverless'
+// Integrations
+import expressiveCode from 'astro-expressive-code'
+import { expressiveCodeOptions } from './src/site.config.ts'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
+import mdx from '@astrojs/mdx'
+import icon from 'astro-icon'
+import playformCompress from '@playform/compress'
+// Markdown
 import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
 import remarkUnwrapImages from 'remark-unwrap-images'
 import rehypeExternalLinks from 'rehype-external-links'
-import expressiveCode from 'astro-expressive-code'
-import { expressiveCodeOptions } from './src/site.config'
-import icon from 'astro-icon'
-
-import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,7 +22,9 @@ export default defineConfig({
   trailingSlash: 'never',
   output: 'server',
   adapter: vercel({
-    webAnalytics: { enabled: true }
+    webAnalytics: {
+      enabled: true
+    }
     // imagesConfig: {
     //   sizes: [320, 640, 1280]
     // }
@@ -33,16 +38,17 @@ export default defineConfig({
     }),
     sitemap(),
     mdx(),
-    icon()
+    icon(),
+    playformCompress()
   ],
   // root: './my-project-directory',
 
   // Prefetch Options
   prefetch: true,
-
   // Server Options
-  server: { host: true },
-
+  server: {
+    host: true
+  },
   // Markdown Options
   markdown: {
     remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
