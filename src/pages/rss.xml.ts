4fda64842bda90b71c0508124b866ddc009dc657
CWorld <cworld0@qq.com>
2024-06-22 21:38:47 +0800

    refactor: :sparkles: rewrite rss feed to support img for content

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,14 +1,51 @@
+import type { AstroGlobal } from 'astro'
+import type { CollectionEntry } from 'astro:content'
 import rss from '@astrojs/rss'
+import { getImage } from 'astro:assets'
+
 import { siteConfig } from '@/site-config'
 import { getAllPosts, sortMDByDate } from '@/utils'
 
+import { parse as htmlParser } from 'node-html-parser'
 import sanitizeHtml from 'sanitize-html'
 import MarkdownIt from 'markdown-it'
 const parser = new MarkdownIt()
 
-export const GET = async () => {
-  const allPosts = await getAllPosts()
-  const allPostsByDate = sortMDByDate(allPosts)
+// get dynamic import of images as a map collection
+const imagesGlob = import.meta.glob<{ default: ImageMetadata }>(
+  '/src/content/post/**/*.{jpeg,jpg,png,gif}' // add more image formats if needed
+)
+
+const renderContent = async (post: CollectionEntry<'post'>, site: URL) => {
+  const html = htmlParser.parse(parser.render(post.body))
+
+  for (const img of html.querySelectorAll('img')) {
+    const src = img.getAttribute('src')!
+    // Relative paths that are optimized by Astro build
+    if (src.startsWith('/images')) {
+      // images starting with `/images/` is the public dir
+      img.setAttribute('src', `${site}${src.replace('/', '')}`)
+    } else {
+      const imagePathPrefix = `/src/content/post/${post.slug}/${src.replace('./', '')}`
+      // call the dynamic import and return the module
+      const imagePath = await imagesGlob[imagePathPrefix]?.()?.then((res) => res.default)
+      if (imagePath) {
+        img.setAttribute(
+          'src',
+          `${site}${(await getImage({ src: imagePath })).src.replace('/', '')}`
+        )
+      }
+    }
+  }
+
+  return sanitizeHtml(html.toString(), {
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+  })
+}
+
+const GET = async (context: AstroGlobal) => {
+  const allPostsByDate = sortMDByDate(await getAllPosts())
+  const siteUrl = context.site ?? new URL(import.meta.env.SITE)
 
   return rss({
     // configs
@@ -20,26 +57,19 @@ export const GET = async () => {
     title: siteConfig.title,
     description: siteConfig.description,
     site: import.meta.env.SITE,
-    items: allPostsByDate.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.publishDate,
-      description: post.data.description,
-      link: `/blog/${post.slug}`,
-      customData: `
+    items: await Promise.all(
+      allPostsByDate.map(async (post) => ({
+        pubDate: post.data.publishDate,
+        link: `/blog/${post.slug}`,
+        customData: `
                 <h:img src="${post.data.coverImage?.src.src}" />
                 <enclosure url="${post.data.coverImage?.src.src}" />
             `,
-      content: sanitizeHtml(parser.render(post.body), {
-        // allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
-        // transformTags: {
-        //   img: (tagName, attribs) => ({
-        //     tagName: tagName,
-        //     attribs: {
-        //       src: `_astro/${attribs.src}`
-        //     }
-        //   })
-        // }
-      })
-    }))
+        content: await renderContent(post, siteUrl),
+        ...post.data
+      }))
+    )
   })
 }
+
+export { GET }
