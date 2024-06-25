c3712e1736199aa0b6cb7217864969aa14eff02f
CWorld <cworld0@qq.com>
2024-06-25 14:43:10 +0800

    feat(pages): :sparkles: update robots.txt to disallow gpt fetching some pages

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,16 +1,21 @@
 import type { APIRoute } from 'astro'
 
 const robotsTxt = `
+User-agent: GPTBot
+User-agent: ClaudeBot
+User-agent: Claude-Web
+Disallow: /blog/
+Allow: /blog/$
+
 User-agent: *
 Allow: /
 
 Sitemap: ${new URL('sitemap-index.xml', import.meta.env.SITE).href}
 `.trim()
 
-export const GET: APIRoute = () => {
-  return new Response(robotsTxt, {
+export const GET: APIRoute = () =>
+  new Response(robotsTxt, {
     headers: {
       'Content-Type': 'text/plain; charset=utf-8'
     }
   })
-}
