c3b4f0811c5dfa19aa4200440604805c6a274e81
CWorld <cworld0@qq.com>
2024-07-06 20:10:03 +0800

    fix(components): :bug: update missing code for linkpreview

--- src/utils/link-preview.ts
+++ src/utils/link-preview.ts
@@ -1,7 +1,71 @@
-import { safeGetDOM } from '@astro-community/astro-embed-utils'
+import { parse as htmlParser } from 'node-html-parser'
+
+class LRU<K, V> extends Map<K, V> {
+  constructor(private readonly maxSize: number) {
+    super()
+  }
+
+  override get(key: K): V | undefined {
+    const value = super.get(key)
+    if (value) this.#touch(key, value)
+    return value
+  }
+
+  override set(key: K, value: V): this {
+    this.#touch(key, value)
+    if (this.size > this.maxSize) this.delete(this.keys().next().value)
+    return this
+  }
+
+  #touch(key: K, value: V): void {
+    this.delete(key)
+    super.set(key, value)
+  }
+}
+
+const formatError = (...lines: string[]) => lines.join('\n         ')
+
+/**
+ * Fetch a URL and parse it as JSON, but catch errors to stop builds erroring.
+ * @param url URL to fetch
+ * @returns {Promise<Record<string, any> | undefined>}
+ */
+export const safeGet = makeSafeGetter<Record<string, any>>((res) => res.json())
+
+/**
+ * Fetch a URL and parse it as HTML, but catch errors to stop builds erroring.
+ * @param url URL to fetch
+ * @returns {Promise<Document | undefined>}
+ */
+export const safeGetDOM = makeSafeGetter(async (res) => htmlParser.parse(await res.text()))
+
+/** Factory to create safe, caching fetch functions. */
+function makeSafeGetter<T>(
+  handleResponse: (res: Response) => T | Promise<T>,
+  { cacheSize = 1000 }: { cacheSize?: number } = {}
+) {
+  const cache = new LRU<string, T>(cacheSize)
+  return async function safeGet(url: string): Promise<T | undefined> {
+    try {
+      const cached = cache.get(url)
+      if (cached) return cached
+      const response = await fetch(url)
+      if (!response.ok)
+        throw new Error(
+          formatError(`Failed to fetch ${url}`, `Error ${response.status}: ${response.statusText}`)
+        )
+      const result = await handleResponse(response)
+      cache.set(url, result)
+      return result
+    } catch (e: any) {
+      console.error(formatError(`[error]  astro-embed`, e?.message ?? e))
+      return undefined
+    }
+  }
+}
 
 /** Helper to get the `content` attribute of an element. */
-const getContent = (el: Element | null) => el?.getAttribute('content')
+const getContent = (el: HTMLElement | null) => el?.getAttribute('content')
 /** Helper to filter out insecure or non-absolute URLs. */
 const urlOrNull = (url: string | null | undefined) => (url?.slice(0, 8) === 'https://' ? url : null)
 
@@ -14,9 +78,9 @@ export async function parseOpenGraph(pageUrl: string) {
   if (!html) return
 
   const getMetaProperty = (prop: string) =>
-    getContent(html.querySelector(`meta[property=${JSON.stringify(prop)}]`))
+    getContent(html.querySelector(`meta[property=${JSON.stringify(prop)}]`) as HTMLElement | null)
   const getMetaName = (name: string) =>
-    getContent(html.querySelector(`meta[name=${JSON.stringify(name)}]`))
+    getContent(html.querySelector(`meta[name=${JSON.stringify(name)}]`) as HTMLElement | null)
 
   const title = getMetaProperty('og:title') || html.querySelector('title')?.textContent
   const description = getMetaProperty('og:description') || getMetaName('description')
