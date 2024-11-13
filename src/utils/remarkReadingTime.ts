8335ed166c473645adbc23a800c8da700dc38602
CWorld <cworld0@qq.com>
2024-11-13 17:03:11 +0800

    build: :rotating_light: update lint code

--- src/utils/remarkReadingTime.ts
+++ src/utils/remarkReadingTime.ts
@@ -2,8 +2,7 @@ import getReadingTime from 'reading-time'
 import { toString } from 'mdast-util-to-string'
 
 export function remarkReadingTime() {
-  // @ts-expect-error:next-line
-  return function (tree, { data }) {
+  return function (tree: any, { data }: { data: any }) {
     const textOnPage = toString(tree)
     const readingTime = getReadingTime(textOnPage)
     // readingTime.text will give us minutes read as a friendly string,
