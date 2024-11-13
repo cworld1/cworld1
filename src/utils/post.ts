b72493cd19eaa52fa11cc16de5511076ed702712
CWorld <cworld0@qq.com>
2024-11-13 20:57:14 +0800

    perf(pages): :zap: won't need to render twice

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -3,7 +3,7 @@ import { getCollection } from 'astro:content'
 
 /** Note: this function filters out draft posts based on the environment */
 export async function getAllPosts() {
-  return await getCollection('post', ({ data }) => {
+  return await getCollection('post', ({ data }: CollectionEntry<'post'>) => {
     return import.meta.env.PROD ? data.draft !== true : true
   })
 }
