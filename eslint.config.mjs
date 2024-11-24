f0151e32df68f0e75db76be150144fc76c7a8d7f
CWorld <cworld0@qq.com>
2024-11-25 00:03:19 +0800

    feat: :test_tube: update eslint with new cnfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,83 +1,18 @@
-import astroParser from 'astro-eslint-parser'
-import tsParser from '@typescript-eslint/parser'
-import astroPlugin from 'eslint-plugin-astro'
-import tsPlugin from '@typescript-eslint/eslint-plugin'
-import js from '@eslint/js'
+// @ts-check
+
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+import eslintPluginAstro from 'eslint-plugin-astro'
 
 export default [
   // For JavaScript and TypeScript files in general
-  {
-    files: ['**/*.{js,cjs,mjs,jsx,ts,tsx}'],
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        window: 'readonly',
-        document: 'readonly',
-        console: 'readonly',
-        fetch: 'readonly',
-        localStorage: 'readonly',
-        setTimeout: 'readonly',
-        HTMLElement: 'readonly',
-        HTMLButtonElement: 'readonly',
-        HTMLElementTagNameMap: 'readonly',
-        URL: 'readonly',
-        Node: 'readonly',
-        Response: 'readonly',
-        module: 'readonly',
-        require: 'readonly'
-      }
-    },
-    plugins: {
-      '@typescript-eslint': tsPlugin
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...tsPlugin.configs.recommended.rules,
-      '@typescript-eslint/no-require-imports': 'off' // allow import using require
-    }
-  },
-  // For Astro files
-  {
-    files: ['**/*.astro'],
-    languageOptions: {
-      parser: astroParser,
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro']
-      },
-      globals: {
-        astro: 'readonly'
-      }
-    },
-    plugins: {
-      astro: astroPlugin,
-      '@typescript-eslint': tsPlugin
-    },
-    rules: {
-      ...astroPlugin.configs.recommended.rules,
-      ...tsPlugin.configs.recommended.rules,
-      'astro/no-unused-css-selector': 'off' // 禁用 CSS 未使用选择器规则
-    }
-  },
-
-  // For Node.js specific configuration
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        require: 'readonly',
-        module: 'readonly',
-        process: 'readonly',
-        __dirname: 'readonly',
-        __filename: 'readonly'
-      }
-    },
-    rules: {}
-  },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
 
   // Ignore files
   {
