f961924172c1b314b9499927085e07ce8e714652
CWorld <cworld0@qq.com>
2024-11-28 01:03:28 +0800

    test(plugins): :test_tube: try to fix remark reading time

--- src/plugins/remarkPlugins.ts
+++ src/plugins/remarkPlugins.ts
@@ -1,14 +1,14 @@
 import getReadingTime from 'reading-time'
 import { toString } from 'mdast-util-to-string'
 
-export function remarkReadingTime() {
-  // We don't know the type of `tree` or `data` here, so we'll use `any`
-  // @ts-expect-error:next-line
+export const remarkReadingTime: Plugin<[], Root> = function () {
   return function (tree, { data }) {
     const textOnPage = toString(tree)
     const readingTime = getReadingTime(textOnPage)
     // readingTime.text will give us minutes read as a friendly string,
     // i.e. "3 min read"
+    const typedData = data as { astro: { frontmatter: { minutesRead: string } } }
+    typedData.astro.frontmatter.minutesRead = readingTime.text
     data.astro.frontmatter.minutesRead = readingTime.text
   }
 }
