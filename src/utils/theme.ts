841ba3938f2e0425af161d6c2a650dac26d4fd0c
CWorld <cworld0@qq.com>
2024-12-03 01:58:18 +0800

    fix(utils): :bug: fix theme change 'system' has instant problem

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -13,29 +13,28 @@ export function setTheme(theme?: string, save = false) {
   const themes = ['system', 'dark', 'light']
   if (theme) {
     if (!themes.includes(theme)) return
-    if (theme === 'system') {
-      theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-    }
     if (save) localStorage.setItem('theme', theme)
   } else {
     theme = getTheme() ?? undefined
-
     if (save) {
-      // System equals null, switch cycle in ['system', 'dark', 'light']
+      // Set theme equals undefined, switch cycle in ['system', 'dark', 'light']
       const currentIndex = themes.indexOf(theme ?? 'system')
       theme = themes[(currentIndex + 1) % themes.length]
       localStorage.setItem('theme', theme) // save theme
     }
   }
+  let targetTheme = theme
+  if (theme === 'system') {
+    targetTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    // Listen theme change
+    listenThemeChange(theme)
+  }
 
   // Set theme
-  document.documentElement.classList.toggle('dark', theme === 'dark')
+  document.documentElement.classList.toggle('dark', targetTheme === 'dark')
   document
     .querySelector('meta[name="theme-color"]')
-    ?.setAttribute('content', theme === 'dark' ? '#0B0B10' : '#FCFCFD')
-
-  // Listen theme change
-  listenThemeChange(theme)
+    ?.setAttribute('content', targetTheme === 'dark' ? '#0B0B10' : '#FCFCFD')
 
   return theme
 }
